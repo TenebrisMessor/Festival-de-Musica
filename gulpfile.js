4026f4788c96fd1fe4452847995ce7e2c02298fd
TenebrisMessor <arielhryo@hotmail.com>
2022-10-08 04:25:49 -0500

    add webp converter at gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,11 @@
 // exports.tarea = tarea;  // exportar la tarea, la parte izquierda esta asociada con el como se va a llamara la tarea en la terminal con node y la de la derecha es el nombre de la funcion que se va a ejecutar
 
 const plumber =require('gulp-plumber'); //requerir el paquete de plumber
-const {src, dest, watch } = require('gulp'); //requerir el paquete de gulp y extraer las funciones src y dest como tal.
+const {src, dest, watch, parallel} = require('gulp'); //requerir el paquete de gulp y extraer las funciones src y dest como tal.
 const sass = require('gulp-sass')(require('sass')); //requerir el paquete de sass
+const webp=require('gulp-webp'); //requerir el paquete de webp
+
+
 function css(done) {
 
     src('src/scss/**/*.scss') //identificar el archivo sass a compilar y todos los archivos que esten dentro de la carpeta scss
@@ -23,8 +26,26 @@ function dev(done) {
     done();
 }
  
+//Imagenes
+
+
+function versionWebp(done) { //funcion para convertir las imagenes a webp
+    const opciones = {
+        quality:50
+    };
+    src('src/img/**/*.{jpg,png}') 
+        .pipe(webp())
+        .pipe(dest('build/img'));
+    done();
+}
+
+
 exports.css=css; //exportar la tarea, la parte izquierda esta asociada con el como se va a llamara la tarea en la terminal con node y la de la derecha es el nombre de la funcion que se va a ejecutar
-exports.dev=dev;
+exports.dev=parallel(versionWebp, dev); //parallel sirve para ejecutar dos o mas tareas simultaneamente
+exports.versionWebp=versionWebp;
+
+
+
 
 
 //para poder utilizar el workflow sin interrupciones hay que instalar el paquete de gulp-plumber y posteriormente utilizarlo en la funcion css en el pipe. 
