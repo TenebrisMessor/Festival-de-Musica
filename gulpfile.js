897c949b1869974a1e1d4beb2dbdf1739b7b830a
TenebrisMessor <arielhryo@hotmail.com>
2022-10-08 04:45:15 -0500

    add automatic avif converter

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,9 @@ const plumber =require('gulp-plumber'); //requerir el paquete de plumber
 const {src, dest, watch, parallel} = require('gulp'); //requerir el paquete de gulp y extraer las funciones src y dest como tal.
 const sass = require('gulp-sass')(require('sass')); //requerir el paquete de sass
 const webp=require('gulp-webp'); //requerir el paquete de webp
-
+const imagemin = require('gulp-imagemin'); //requerir el paquete de imagemin
+const cache = require('gulp-cache'); //requerir el paquete de cache
+const avif = require('gulp-avif'); //requerir el paquete de avif    
 
 function css(done) {
 
@@ -22,14 +24,31 @@ function css(done) {
 }
 
 //Imagenes
-
+function imagenes(done){
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg,jpeg}')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest('build/img'));
+    done();
+}
 
 function versionWebp(done) { //funcion para convertir las imagenes a webp
     const opciones = {
         quality:50
     };
     src('src/img/**/*.{jpg,png}') 
-        .pipe(webp())
+        .pipe(webp(opciones))
+        .pipe(dest('build/img'));
+    done();
+}
+function versionAvif(done) { //funcion para convertir las imagenes a webp
+    const opciones = {
+        quality:50
+    };
+    src('src/img/**/*.{jpg,png}') 
+        .pipe(avif(opciones))
         .pipe(dest('build/img'));
     done();
 }
@@ -43,8 +62,10 @@ function dev(done) {
 
 
 exports.css=css; //exportar la tarea, la parte izquierda esta asociada con el como se va a llamara la tarea en la terminal con node y la de la derecha es el nombre de la funcion que se va a ejecutar
-exports.dev=parallel(versionWebp, dev); //parallel sirve para ejecutar dos o mas tareas simultaneamente
+exports.imagenes=imagenes;
 exports.versionWebp=versionWebp;
+exports.versionAvif=versionAvif;    
+exports.dev=parallel(imagenes,versionAvif, versionWebp, dev); //parallel sirve para ejecutar dos o mas tareas simultaneamente
 
 
 
